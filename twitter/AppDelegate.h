d261c2b0fd7d647c232fe33451d1c2ab70f4d69e
Rajeev Nayak <jeev.nayak@gmail.com>
2014-06-19 22:04:55 -0700

    Initial Commit

---
+++ twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twitter
+//
+//  Created by Rajeev Nayak on 6/19/14.
+//  Copyright (c) 2014 jeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  twitter
+//
+//  Created by Rajeev Nayak on 6/19/14.
+//  Copyright (c) 2014 jeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
